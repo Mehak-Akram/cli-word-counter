d0c7b4ee87081e35dc27ac009c555b0e39481953
Mehak-Akram <akramabbasi019@gmail.com>
2024-05-13 19:46:48 +0500

    push my word-counter to github

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.cyanBright("\n\t\tWORD COUNTER"))
+
+console.log("=".repeat(60))
+
+const anwers : {
+     sentence : string
+} = await inquirer.prompt([{
+    name : "sentence", 
+    types : "input",
+    message : "Enter you sentence to count the words"
+}])
+
+
+const words = anwers.sentence.trim().split(" ")
+
+console.log("=".repeat(60))
+
+console.log(words);
+
+console.log(chalk.bold.blue(`you sentence words counts is: ${words.length}`))
+
+console.log("=".repeat(60))
+
